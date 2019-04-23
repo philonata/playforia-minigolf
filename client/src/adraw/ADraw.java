b94f3538b715baea265d48b5b3574c170c4f3eff
PhilippvK <philipp.van-kempen@tum.de>
2019-04-23 20:03:50 +0200

    remove other game stuff

--- client/src/adraw/ADraw.java
+++
@@ -1,9 +0,0 @@
-package adraw;
-
-public class ADraw extends GameApplet {
-
-    public String getAppletInfo() {
-        return "-= ADraw =-\nCopyright (c) 2004-2010 Apaja (www.apaja.com)\nProgramming: Pasi Laaksonen";
-    }
-
-}
