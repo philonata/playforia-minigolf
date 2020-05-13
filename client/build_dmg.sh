73c46457a9aa279ef54044891444f47bbac3a4ff
Philipp van Kempen <philipp.van-kempen@tum.de>
2020-05-13 17:22:45 +0200

    big update

---
+++ client/build_dmg.sh
@@ -0,0 +1,9 @@
+#~/bin/bash
+rm -f minigolf_setup.dmg
+dd if=/dev/zero of=minigolf_setup.dmg bs=1M count=256 status=progress
+mkfs.hfsplus -v Installer minigolf_setup.dmg
+mkdir -p ./tmp
+sudo mount minigolf_setup.dmg ./tmp/
+sudo cp -r Client.app/ ./tmp/Client.app
+sudo umount ./tmp
+rm tmp
