82c7a19f89656c463e339ea8762c1820c973b235
Philipp van Kempen <phvankempen@googlemail.com>
2020-05-13 07:13:49 +0200

    Started working on standalone binaries

---
+++ client/build_img.sh
@@ -0,0 +1,7 @@
+#~/bin/bash
+which genisoimage || echo "Install genisoimage first!"
+rm -f minigolf_setup.img
+mkdir -p /tmp/disk
+rm -rf /tmp/disk/*
+cp -r Client.app/ /tmp/disk/Client.app
+genisoimage -o minigolf_setup.img -D -r -l -ldots -V "Installer" /tmp/disk
