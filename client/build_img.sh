52ec29327d77413d777c2a3a79f4cf31052be6d7
Philipp van Kempen <philipp.van-kempen@tum.de>
2020-05-14 07:22:38 +0200

    rrmoved

--- client/build_img.sh
+++ client/build_img.sh
@@ -4,4 +4,4 @@ rm -f minigolf_setup.img
 mkdir -p /tmp/disk
 rm -rf /tmp/disk/*
 cp -r Client.app/ /tmp/disk/Client.app
-genisoimage -o minigolf_setup.img  -r -l -ldots -V "Installer" /tmp/disk
+genisoimage -o minigolf_setup.img -D -r -l -ldots -V "Installer" /tmp/disk
